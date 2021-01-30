f5b5be8c8c57583911dac868603cd57e02ff0e0d
Josh Frank <eurosewusa@gmail.com>
2021-01-30 13:45:28 -0500

    commit tree 20f17683c87396badb8f495c9b1b71da23358b95

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+
+let dodger = document.getElementById( "dodger" )
+let playfield = document.getElementById( "game" )
+
+document.addEventListener( "keydown", function( event ) {
+    if ( event.key == "ArrowLeft" ) { moveDodgerLeft(); }
+    if ( event.key == "ArrowRight" ) { moveDodgerRight(); }
+    if ( event.key == "ArrowUp" ) { moveDodgerUp(); }
+    if ( event.key == "ArrowDown" ) { moveDodgerDown(); }
+} );
+
+function moveDodgerLeft() {
+    let currentPosition = parseInt( dodger.style.left.slice( 0,-2 ) );
+    if ( currentPosition > 0 ) { dodger.style.left = `${ currentPosition - 5 }px`; }
+}
+
+function moveDodgerRight() {
+    let currentPosition = parseInt( dodger.style.left.slice( 0,-2 ) );
+    if ( currentPosition < 380 ) { dodger.style.left = `${ currentPosition + 5 }px`; }
+}
+
+function moveDodgerUp() {
+    let currentPosition = parseInt( dodger.style.bottom.slice( 0,-2 ) );
+    if ( currentPosition < 380 ) { dodger.style.bottom = `${ currentPosition + 5 }px`; }
+}
+
+function moveDodgerDown() {
+    let currentPosition = parseInt( dodger.style.bottom.slice( 0,-2 ) );
+    if ( currentPosition > 0 ) { dodger.style.bottom = `${ currentPosition - 5 }px`; }
+}
